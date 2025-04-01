936edc288aa3d860f9dd3ee1193c8f8588ad13c4
Amal-els <amalbahri19@gmail.com>
2025-04-01 16:46:20 +0100

    3

--- ex1/uploads/acceuil.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pageName = "Acceuil";
-include 'fragments/header.php';
-?>
-<form action="index.php" method = "post">
-    Saisir les coordonnées des etudiants (séparés par /): <input type="text" name = "etuds">
-    <input type="submit" value = "submit">
-</form>
